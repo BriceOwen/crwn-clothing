b0378b0e6960e326379a38874705d4c4ee996f8d
Owen Brice <owenbriceind@gmail.com>
2020-04-15 11:29:47 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import Home from './pages/home/home.component';
 import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/shop' component={Shop} />
